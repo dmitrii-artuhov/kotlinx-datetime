dd58ab8c85e1743b2c424cbb6183ed37dcb8ca4e
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-11-13 18:05:11 +0100

    Decrease iteration count for `instant` test

--- core/jvm/test/ConvertersTest.kt
+++ core/jvm/test/ConvertersTest.kt
@@ -30,7 +30,7 @@ class ConvertersTest {
             assertEquals(jtInstant, ktInstant.toString().let(JTInstant::parse))
         }
 
-        repeat(STRESS_TEST_ITERATIONS) {
+        repeat(/*STRESS_TEST_ITERATIONS*/ 3) {
             val seconds = Random.nextLong(1_000_000_000_000)
             val nanos = Random.nextInt()
             test(seconds, nanos)
